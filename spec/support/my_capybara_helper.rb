0448ffdc046c606bb02bb549600ac45cdcad3f38
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-20 12:15:05 +0000

    Create a helper for Capybara tests to make them more readable.

---
+++ spec/support/my_capybara_helper.rb
@@ -0,0 +1,8 @@
+module MyCapybaraHelper
+
+  #thanks to Yanis Triandaphilov for the recommendation, sourced from his blog
+  #https://blog.codeship.com/principles-of-effective-testing-with-capybara/
+  def should_see(text)
+    expect(page).to have_content(text)
+  end
+end
