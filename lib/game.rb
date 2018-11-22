215415cc589038e543806849ba57f6f737baeb15
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-22 17:48:55 +0000

    Create spec for #initialize and implement behaviour.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,15 @@
 class Game
 
+  def initialize(player_1 = Player.new, player_2 = Player.new)
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def player_1
+    @player_1
+  end
+
+  def player_2
+    @player_2
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,16 @@
 require 'game'
 
 describe Game do
-  let(:game) { described_class.new }
+  let(:game) { described_class.new(player_klass, other_player_klass) }
+  let(:player_klass) { double('Player 1') }
+  let(:other_player_klass) { double('Player 2') }
+
+  context 'on initialization' do
+    it 'has a player 1' do
+      expect(game.player_1).to eq(player_klass)
+    end
+    it 'has a player 2' do
+      expect(game.player_2).to eq(other_player_klass)
+    end
+  end
 end
