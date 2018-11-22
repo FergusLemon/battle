7f4cef15b2df3be523a9f4e4f2300b4ea1571ce5
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-22 15:25:09 +0000

    Create feature test to check that after an attack players can return to the battleground.

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,12 @@
 feature 'Attacking opponents' do
   scenario 'Players can attack each other' do
-    enter_names('Jane', 'Peter')
-    click_on 'Attack'
+    initiate_an_attack
     should_see 'Good Hit!'
+  end
+
+  scenario 'After an attack return to battle ground' do
+    initiate_an_attack
+    click_on 'Back To Battle'
+    should_see 'Player 1 Hit Points'
   end
 end
