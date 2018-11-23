da94a48c3b338a815c7a35aaa72d20f284e0e80f
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-23 12:37:21 +0000

    Implement #attacker, #defender and update #attack to switch turns after each attack.

--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,39 @@
 class Game
 
   def initialize(player_1 = Player.new, player_2 = Player.new)
-    @player_1 = player_1
-    @player_2 = player_2
+    @players = [player_1, player_2]
+    @attacker = @players.first
+    @defender = @players.last
+  end
+
+  def players
+    @players.dup
   end
 
   def player_1
-    @player_1
+    @players.first
   end
 
   def player_2
-    @player_2
+    @players.last
+  end
+
+  def attacker
+    @attacker
+  end
+
+  def defender
+    @defender
   end
 
   def attack(player)
     player.incur_damage
+    switch_turns
+  end
+
+  private
+
+  def switch_turns
+    @attacker, @defender = @defender, @attacker
   end
 end
