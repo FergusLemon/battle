d6d84c69c708ab5a5c0aa9ec9099a2025bb1f66f
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-23 10:18:56 +0000

    Refactor away global varaibles on players.

--- app.rb
+++ app.rb
@@ -13,24 +13,24 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
-    $game = Game.new($player_1, $player_2)
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1.name
-    @player_2 = $player_2.name
-    @player_1_hit_points = $player_1.hit_points
-    @player_2_hit_points = $player_2.hit_points
+    @player_1 = $game.player_1.name
+    @player_2 = $game.player_2.name
+    @player_1_hit_points = $game.player_1.hit_points
+    @player_2_hit_points = $game.player_2.hit_points
     erb :play
   end
 
   get '/attack' do
     session[:message] = 'Good Hit!'
     @message = session[:message]
-    $game.attack($player_2)
+    $game.attack($game.player_2)
     erb :attack
   end
 end
