c2b57379a3775fc4438fd6936bcb78fafe356b1d
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-23 12:35:24 +0000

    Update for change in helper methods.

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,18 +1,19 @@
 feature 'Attacking opponents' do
   scenario 'Players can attack each other' do
-    initiate_an_attack
+    enter_names
+    click_on 'Attack'
     should_see 'Good Hit!'
   end
 
   scenario 'After an attack return to battle ground' do
-    initiate_an_attack
-    click_on 'Back To Battle'
+    enter_names
+    attack_and_return
     should_see 'Player 1 Hit Points'
   end
 
   scenario "An attack reduces a player's hit points" do
-    initiate_an_attack
-    click_on 'Back To Battle'
+    enter_names
+    attack_and_return
     should_see 'Player 2 Hit Points = 90'
   end
 end
