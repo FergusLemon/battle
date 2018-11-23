9184332150277aa7b4d8b107d9870c737c5b4c1e
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-23 12:36:23 +0000

    Create specs for #attacker, #defender and new behaviour of #attack.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,24 +1,35 @@
 require 'game'
 
 describe Game do
-  let(:game) { described_class.new(player_klass, other_player_klass) }
-  let(:player_klass) { double('Player 1') }
-  let(:other_player_klass) { double('Player 2') }
+  let(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double('Player 1') }
+  let(:player_2) { double('Player 2') }
 
   context 'on initialization' do
     it 'has a player 1' do
-      expect(game.player_1).to eq(player_klass)
+      expect(game.players.first).to eq(player_1)
     end
     it 'has a player 2' do
-      expect(game.player_2).to eq(other_player_klass)
+      expect(game.players.last).to eq(player_2)
+    end
+    it 'has an attacker' do
+      expect(game.attacker).to eq(player_1)
+    end
+    it 'has an defender' do
+      expect(game.defender).to eq(player_2)
     end
   end
 
   describe '#attack' do
-    it 'it sends a message to players' do
-      allow(player_klass).to receive(:incur_damage)
-      expect(player_klass).to receive(:incur_damage)
-      game.attack(player_klass)
+    it 'sends a message to players' do
+      allow(player_1).to receive(:incur_damage)
+      expect(player_1).to receive(:incur_damage)
+      game.attack(player_1)
+    end
+    it 'switches the active player' do
+      allow(player_2).to receive(:incur_damage)
+      game.attack(player_2)
+      expect(game.attacker).to eq(player_2)
     end
   end
 end
