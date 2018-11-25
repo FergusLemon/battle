e663f320a05d135fa7e8efd7802ea8904454a5df
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-25 19:50:29 +0000

    Remove unused variable.

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
-    game = Game.new(player_1, player_2)
+    Game.new(player_1, player_2)
     redirect '/play'
   end
 
