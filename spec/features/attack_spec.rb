f869ae00411eab0af552c99b6235e7db04cd0c68
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-26 09:17:35 +0000

    Update feature test to account for randomness of damage incurred.

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -14,6 +14,6 @@ feature 'Attacking opponents' do
   scenario "An attack reduces a player's hit points" do
     enter_names
     attack_and_return
-    should_see 'Player 2 Hit Points = 90'
+    should_not_see 'Player 2 Hit Points = 100'
   end
 end
