9052e6ce862335cd83f993a14f9865a01cb4ed99
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-26 10:22:55 +0000

    Refactor feature test to stub out random damage points and test that result page is shown when a game is over.

--- spec/features/losing_a_game_spec.rb
+++ spec/features/losing_a_game_spec.rb
@@ -1,9 +1,8 @@
 feature 'Losing a game' do
   scenario 'Player 2 loses the game' do
-    enter_names('Anna', 'Ben')
-    19.times do
-      attack_and_return
-    end
-    should_see 'Ben has 0 Hit Points left. Anna wins!'
+    allow_any_instance_of(Game).to receive(:over?)\
+      .and_return(true)
+    enter_names
+    should_see '0 Hit Points left.'
   end
 end
