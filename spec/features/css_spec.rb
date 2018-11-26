f6fdfb5eeff14e9a229723ecabbbff2e2d180a3c
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-26 14:32:10 +0000

    Test for and add crimson background color to battleground.

--- spec/features/css_spec.rb
+++ spec/features/css_spec.rb
@@ -1,6 +1,13 @@
 feature 'Colours' do
   scenario 'homepage background is neutral colour' do
     visit '/'
-    expect(page).to have_css('body')
+    expect(page).to have_css('div.player-1')
+    expect(page).to have_css('div.player-2')
+  end
+
+  scenario 'battle page background is a crimson colour' do
+    enter_names
+    expect(page).to have_css('div.player-1-info')
+    expect(page).to have_css('div.player-2-info')
   end
 end
