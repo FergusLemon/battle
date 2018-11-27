e38114200275c90b7a1cfcdef93f2c8740151e5c
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-27 12:20:14 +0000

    Create feature spec and implement page title.

--- spec/features/losing_a_game_spec.rb
+++ spec/features/losing_a_game_spec.rb
@@ -1,8 +1,14 @@
 feature 'Losing a game' do
+  before do
+    allow_any_instance_of(Game).to receive(:over?).and_return(true)
+  end
   scenario 'Player 2 loses the game' do
-    allow_any_instance_of(Game).to receive(:over?)\
-      .and_return(true)
     enter_names
     should_see '0 Hit Points left.'
+  end
+
+  scenario 'Has a branded header' do
+    enter_names
+    should_see 'Battleground II'
   end
 end
