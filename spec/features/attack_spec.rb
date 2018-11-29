848f3da8beac6439792a1d3316c274aa920007bd
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-29 13:58:28 +0000

    Include new feature test to add coverage for a user story and add the user stories as comments.

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,3 +1,10 @@
+# As Player 1,
+# So I can win a game of Battle,
+# I want to attack Player 2, and I want to get a confirmation
+#
+# As Player 1,
+# So I can lose a game of Battle,
+# I want Player 2 to attack me, and I want to get a confirmation
 feature 'Attacking opponents' do
   scenario 'Players can attack each other' do
     enter_names
@@ -12,10 +19,22 @@ feature 'Attacking opponents' do
     should_see 'Player 2:'
   end
 
+# As Player 1,
+# So I can start to win a game of Battle,
+# I want my attack to reduce Player 2's HP by 10
   scenario "An attack reduces a player's hit points" do
     enter_names
     attack_and_return
     should_not_see 'Player 2 Hit Points = 100'
+  end
+
+# As Player 1,
+# So I can start to lose a game of Battle,
+# I want Player 2's attack to reduce my HP by 10
+  scenario "An attack reduces a player's hit points" do
+    enter_names
+    2.times { attack_and_return }
+    should_not_see 'Player 1 Hit Points = 100'
   end
 
   scenario 'Has a branded header' do
