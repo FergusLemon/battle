dc61796e7e10127e37c2a8c6a98d009f0fdd729c
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-29 12:58:22 +0000

    Update spec for new text.

--- spec/features/losing_a_game_spec.rb
+++ spec/features/losing_a_game_spec.rb
@@ -10,7 +10,7 @@ feature 'Losing a game' do
 
   scenario 'Player 2 loses the game' do
     enter_names_and_attack
-    should_see '0 Hit Points left.'
+    should_see 'Game Over'
   end
 
   scenario 'Has a branded header' do
