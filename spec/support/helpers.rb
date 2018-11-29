5ea07c24b07d55b3727b3826661541eeebce5ab5
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-29 11:08:24 +0000

    Create new helper to DRY out code.

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -7,6 +7,11 @@ module MyCapybaraHelper
     click_on 'Submit'
   end
 
+  def enter_names_and_attack(*names)
+    enter_names(*names)
+    click_on 'Attack'
+  end
+
   def attack_and_return
     click_on 'Attack'
     click_on 'Back To Battle'
