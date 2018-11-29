58e0f34b77034cc929065cbfb8a9c89011dfc4e6
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-29 11:08:06 +0000

    Update specs for new routing behaviour.

--- spec/features/losing_a_game_spec.rb
+++ spec/features/losing_a_game_spec.rb
@@ -2,18 +2,24 @@ feature 'Losing a game' do
   before do
     allow_any_instance_of(Game).to receive(:over?).and_return(true)
   end
-  scenario 'Player 2 loses the game' do
+
+  scenario 'A game is lost' do
     enter_names
+    should_see '0'
+  end
+
+  scenario 'Player 2 loses the game' do
+    enter_names_and_attack
     should_see '0 Hit Points left.'
   end
 
   scenario 'Has a branded header' do
-    enter_names
+    enter_names_and_attack
     should_see 'Battleground II'
   end
 
   scenario 'Starting a new game' do
-    enter_names
+    enter_names_and_attack
     click_on 'Play Again'
     should_see 'Welcome'
   end
